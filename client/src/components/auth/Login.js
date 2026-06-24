d2b4be271f6d97c8a97a12b68f0856aedc55690b
Pro-Novice <projat_banerjee@hotmail.com>
2026-06-24 15:53:46 +0530

    Configuring Registration and Login Forms

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,54 @@
-import React from 'react'
+// import React from 'react'
+
+// const Login = () => {
+//   return (
+//     <div>
+//       Login
+//     </div>
+//   )
+// }
+
+// export default Login
+
+// Hooks are new in React 16.8.  A Hook is a special type of function that allows us to hook into React features.
+// useState is a Hook that allows us to add React state to function components. Prior to Hooks, if we were to write a function
+// component and later had to add state to it, we had to convert it to a class, but now we can use a Hook inside the function component.
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [formData, updateFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  // onChange handler configured to handle change for all fields
+  const onChange = e => updateFormData({ ...formData, [e.target.name]: e.target.value });
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log('awesome!');
+  };
+
   return (
     <div>
-      Login
+      <h1 className="large text-primary">Log in</h1>
+      <p className="cta"><i className="fas fa-sign-in-alt"></i> Log in to your account</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
+        </div>
+        <div className="form-group">
+          <input type="password" placeholder="Password" name="password" value={password} onChange={e => onChange(e)} minLength="4" />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="m">
+        Not registered yet? <Link to='/register'>Register now</Link>
+      </p>
     </div>
   )
-}
+};
 
-export default Login
+export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,72 @@
-import React from 'react'
+// import React from 'react'
+
+// const Register = () => {
+//   return (
+//     <div>
+//       Register  
+//     </div>
+//   )
+// }
+
+// export default Register
+
+// Hooks are new in React 16.8.  A Hook is a special type of function that allows us to hook into React features.
+// useState is a Hook that allows us to add React state to function components. Prior to Hooks, if we were to write a function
+// component and later had to add state to it, we had to convert it to a class, but now we can use a Hook inside the function component.
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Register = () => {
+  const [formData, updateFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmpw: ''
+  });
+
+  const { name, email, password, confirmpw } = formData;
+ 
+  // onChange handler configured to handle change for all fields
+  const onChange = e => updateFormData({
+    ...formData,
+    [e.target.name]: e.target.value
+  });
+ 
+  // onSubmit handler
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== confirmpw) {
+      console.log('Passwords must match');
+    } else {
+      console.log(formData);
+    }
+  }
+
   return (
     <div>
-      Register  
+      <h1 className="large text-primary">Register</h1>
+      <p className="cta"><i className="fas fa-address-card"></i> Specify your account info</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+        </div>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
+        </div>
+        <div className="form-group">
+          <input type="password" placeholder="Password" name="password" value={password} onChange={e => onChange(e)} minLength="4" />
+        </div>
+        <div className="form-group">
+          <input type="password" placeholder="Confirm Password" name="confirmpw" value={confirmpw} onChange={e => onChange(e)} minLength="4" />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="m">
+        Already have an account? <Link to='/login'>Log In</Link>
+      </p>
     </div>
   )
-}
+};
 
-export default Register
+export default Register;
